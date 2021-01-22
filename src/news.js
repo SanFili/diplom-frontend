9fbbdc23e6659f44ec1a259428ed442a1a09be7f
SanFili <filimonova.alexandra@yandex.ru>
2021-01-23 00:29:44 +0300

    страница с сохраненными статьями

--- src/news.js
+++ src/news.js
@@ -1,2 +1,25 @@
 import "./pages/news.css";
 import MainApi from "./js/api/MainApi";
+import NewsApi from "./js/api/NewsApi";
+import NewsCard from "./js/components/NewsCard";
+import NewsCardList from "./js/components/NewsCardList";
+
+const headerBtn = document.querySelector('.header__button');
+
+//api
+const apiMain = new MainApi();
+const news = new NewsApi();
+//классы
+const cardClass = new NewsCard();
+const newsList = new NewsCardList(news, apiMain, cardsContainer, cardClass);
+
+newsList.renderSavedPage();
+
+headerBtn.textContent = localStorage.getItem("username");
+
+function clearStorage() {
+  localStorage.clear();
+  window.location.href = "index.html";
+}
+
+headerBtn.addEventListener("click", clearStorage);
